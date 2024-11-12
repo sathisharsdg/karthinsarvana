142f678b885f55a7708be0f9b38f13014b9db22c
sathisharsdg <sathish.333ddd@gmail.com>
2024-11-12 19:04:57 +0530

    changes from srvana

--- cypress/e2e/.sathish.cy.js
+++ cypress/e2e/.sathish.cy.js
@@ -3,8 +3,8 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io');
     cy.visit('https://www.google.com/');
     cy.visit('https://www.gmail.com/');
-     cy.get(".sample2").click();
-    cy.get(".sample").click();
+     cy.get(".sample222").click();
+    cy.get(".sample111").click();
     
   })
 })
