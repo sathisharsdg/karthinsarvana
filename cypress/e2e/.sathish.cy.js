a890af6852b153cae5160bd6bc3ab5cdb9cae2ca
sathisharsdg <sathish.333ddd@gmail.com>
2024-11-12 19:00:24 +0530

    changes

--- cypress/e2e/.sathish.cy.js
+++ cypress/e2e/.sathish.cy.js
@@ -2,6 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io');
     cy.visit('https://www.google.com/');
+    cy.visit('https://www.gmail.com/');
     
   })
 })
